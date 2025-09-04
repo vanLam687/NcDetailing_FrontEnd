45e150c04113cfa56fc8a95ebf10d75e7e610e76
vanLam687 <vnlamashinojosa@fecea.edu.ar>
2025-09-04 11:11:46 -0300

    feat: Added login and JWT services, and implemented the container and navbar
    Imported HttpClientModule and FormsModule in app.module, created the login and JWT handling services, and created the Container and Navbar components.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,12 +1,17 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ContainerComponent } from './Components/container/container.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ContainerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.component.css'
 })
 export class App {
   protected title = 'NcDetailing.App';
