22be4cf7a246f93896c7e077b5b54a57e13cf771
vanLam687 <vnlamashinojosa@fecea.edu.ar>
2025-09-04 14:05:21 -0300

    feat(router): add project base routes

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LockComponent } from './Components/lock/lock.component';
+import { ContainerComponent } from './Components/container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+    { path: "login", component: LoginComponent},
+    { path: "register", component: RegisterComponent },
+    { path: "lock", component: LockComponent},
+    { path: "home", component: ContainerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
