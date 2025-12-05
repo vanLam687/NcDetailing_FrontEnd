8719f14eaa828dd10905fc0fe20b1ba01ecbfa61
Facundo Castellano <castellanofacundo05@gmail.com>
2025-12-05 11:43:23 -0300

    Use deployed backend url

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://ncdetailing.up.railway.app/api'
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://ncdetailing.up.railway.app/api'
+  //apiUrl: 'http://localhost:3000/api'
 };
