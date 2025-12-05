d0259a9f0a808422c72cfafadecab946c18ce938
Facundo Castellano <castellanofacundo05@gmail.com>
2025-12-05 12:27:21 -0300

    fix environment imports

--- src/app/Services/clients-service.ts
+++ src/app/Services/clients-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth-service';
+
 import { environment } from '../../environments/environment';
 
 @Injectable({
--- src/app/Services/dashboard-service.ts
+++ src/app/Services/dashboard-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth-service';
 import { Router } from '@angular/router';
+
 import { environment } from '../../environments/environment';
 
 @Injectable({
